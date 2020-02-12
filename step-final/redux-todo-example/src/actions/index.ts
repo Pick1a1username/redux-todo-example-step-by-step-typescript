b3bae68cc2e93818dca29a27d73aa231aa3a736c
Pick1a1username <20301273+Pick1a1username@users.noreply.github.com>
2020-02-12 22:34:18 +0900

    [step-final] files copied from step-2

---
+++ step-final/redux-todo-example/src/actions/index.ts
@@ -0,0 +1,17 @@
+import { actionCreatorFactory } from 'typescript-fsa';
+
+const actionCreator = actionCreatorFactory()
+
+// id cannot be set in this code....
+// So it is moved to the reducer.
+// let nextTodoId = 0
+
+// export const addTodo = text => ({
+//   type: 'ADD_TODO',
+//   id: nextTodoId++,
+//   text
+// })
+
+export const addTodo = actionCreator<string>('ADD_TODO')
+
+export const toggleTodo = actionCreator<number>('TOGGLE_TODO')
