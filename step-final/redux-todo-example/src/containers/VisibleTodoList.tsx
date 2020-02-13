b5932eca9af1b1f5012397fa72352f39e0a4f2fc
Pick1a1username <20301273+Pick1a1username@users.noreply.github.com>
2020-02-13 22:56:00 +0900

    [step-final] step-final is now functional.

--- step-final/redux-todo-example/src/containers/VisibleTodoList.tsx
+++ step-final/redux-todo-example/src/containers/VisibleTodoList.tsx
@@ -4,8 +4,27 @@ import TodoList from '../components/TodoList'
 import { Dispatch } from "redux";
 
 import { AppState } from "../store";
-
 import { toggleTodo } from '../actions'
+
+type Todo = {
+  id: number,
+  text: string,
+  completed: boolean
+}
+
+const getVisibleTodos = (todos: Todo[], filter: string) => {
+  switch (filter) {
+    case 'SHOW_ALL':
+      return todos
+    case 'SHOW_COMPLETED':
+      return todos.filter(t => t.completed)
+    case 'SHOW_ACTIVE':
+      return todos.filter(t => !t.completed)
+    // This 'default' is important!!!
+    default:
+      return todos
+  }
+}
 
 export interface VisibleTodoListHandler {
   onTodoClick(id: number): void
@@ -13,7 +32,7 @@ export interface VisibleTodoListHandler {
 
 const mapStateToProps = (appState: AppState) => {
   return {
-    todos: appState.todosReducer
+    todos: getVisibleTodos(appState.todosReducer, appState.visibilityFilterReducer)
   }
 }
 
